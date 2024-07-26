af279b509389b5173d95bd89f88801e89a31fa94
victtows <josevictorjustinosilva@gmail.com>
2024-07-26 10:37:25 -0300

    vue_3_mundo_real - parte 4

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -8,12 +8,11 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/">Events</RouterLink> |
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
     </header>
-    <h1>Events For Good</h1> <!-- new element -->
     <RouterView />
   </div>
 </template>
